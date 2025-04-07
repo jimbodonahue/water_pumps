afa3999f26c5038fe521093c7793a3b3e3943968
Fatemeh Ebrahimi <parastoo_phys@yahoo.com>
2025-04-07 10:06:12 +0200

    updating cleaning

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -14,14 +14,27 @@ train = pd.read_csv(os.path.join(path, 'train.csv'))
 test = pd.read_csv(os.path.join(path, 'test.csv'))
 labels = pd.read_csv(os.path.join(path, 'train_labels.csv'))
 
+#Initial exploring,
 print("Train shape:", train.shape)
-print("Test shape:", test.shape)
-print("Labels shape:", labels.shape)
-
+print("Preview of train data:")
+print(train.head(10))
 print("\nTrain columns:")
 print(train.columns)
 
-#Initial exploring, missing values
+print("Test shape:", test.shape)
+print("Labels shape:", labels.shape)
+print("Preview of labels:")
+print(labels.head())
+
 print(train.dtypes.value_counts())
 print(train.describe().T)
+
+# Replace 0s with NaN where 0 means "missing" or invalid
+df = train.merge(labels, on='id') # Merge tain and lable
+df['construction_year'] = df['construction_year'].replace(0, np.nan)
+df['gps_height'] = df['gps_height'].replace([0, -90], np.nan)
+df['longitude'] = df['longitude'].replace(0, np.nan)
+print("Missing values after cleaning invalid numeric entries:")
+print(df[['construction_year', 'gps_height', 'longitude']].isnull().sum())
+
 
